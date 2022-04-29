d12f5ab4313fcb171fe4b28872bb46e3e759d06d
Evan Laverdiere <0860347@johnabbottcollege.net>
2022-04-29 11:55:20 -0400

    Started working on implementation of get-one song method.

--- models/songModelMySql.js
+++ models/songModelMySql.js
@@ -1,7 +1,21 @@
 
 
-function getOneSong(){}
+function getOneSong(userId, title, artist, genre, album) {
+    // TO-DO: Validate that passed userId
+    
+    const query = 'SELECT * FROM songs ' +
+        'WHERE title = \'' + title + '\' ' +
+        'AND artist = \'' + artist + '\' ' +
+        'AND genre = \'' + genre + '\' ' +
+        'AND userId = ' + userId;
 
-function updateSong(){}
+    
+}
 
-function deleteSong(){}
+//#region UPDATE Operations
+function updateSong() { }
+//#endregion
+
+//#region DELETE Operations
+function deleteSong() { }
+//#endregion
