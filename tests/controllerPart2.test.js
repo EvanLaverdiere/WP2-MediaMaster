2c672c1aabdfc6f3d11b66917e2f2c692d97605e
Evan Laverdiere <78218044+EvanLaverdiere@users.noreply.github.com>
2022-05-08 14:50:02 -0400

    3.4.2: Created test file for songController RUD operations.

---
+++ tests/controllerPart2.test.js
@@ -0,0 +1,64 @@
+const app = require('../app');
+const supertest = require('supertest');
+const testRequest = supertest(app);
+const errorTypes = require('../models/errorModel');
+
+// Initialise the test database before each test.
+const dbName = "mediamaster_db_test";
+const songsModel = require('../models/songModelMySql');
+const usersModel = require('../models/userModelMySql');
+const logger = require('../logger');
+
+beforeEach(async () => {
+    try {
+        await songsModel.initialize(dbName, true);
+    }
+    catch (error) {
+        // Fail gracefully.
+    }
+});
+
+// Close database after each test.
+afterEach(async () => {
+    connection = songsModel.getConnection();
+    if (connection) {
+        await connection.close();
+    }
+});
+
+const userData = [
+    { userId: 1, username: "Bob", password: "p@ssW0rD" },
+    { userId: 2, username: "MusicLover95", password: "I_luv_songs" }
+]
+
+/* data to be used to generate random songs for testing. */
+const songData = [
+    { title: "Beat It", artist: "Michael Jackson", genre: "Pop", album: "Thriller" },
+    { title: "The Girl is Mine", artist: "Michael Jackson", genre: "Pop", album: "Thriller" },
+    { title: "Thriller", artist: "Michael Jackson", genre: "Pop", album: "Thriller" },
+    { title: "Fly Like an Eagle", artist: "Seal", genre: "rhythm", album: "Space Jam" },
+    { title: "My Girl", artist: "The Temptations", genre: "soul" },
+    { title: "The Way You Do the Things You Do", artist: "The Temptations", genre: "soul" }
+]
+
+const generateUserData = () => {
+    const index = Math.floor(Math.random() * userData.length);
+    return userData.slice(index, index + 1)[0];
+}
+
+const generateSongData = () => {
+    const index = Math.floor(Math.random() * songData.length);
+    return songData.slice(index, index + 1)[0];
+}
+
+//#region Controller GET Tests
+
+//#endregion
+
+//#region Controller UPDATE Tests
+
+//#endregion
+
+//#region Controller DELETE Tests
+
+//#endregion
